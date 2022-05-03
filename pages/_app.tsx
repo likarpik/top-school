aaec7960878a61c9a153ccd7038b4cfe73486c10
Anzhelika Karpik <karpik.lika@mail.ru>
2022-05-03 18:05:42 +0300

    Add Htag component for generations html tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <title>Top School</title>
+      <link rel="icon" href="/public/favicon.ico" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600&display=swap" rel="stylesheet"></link>
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
 
 export default MyApp;
