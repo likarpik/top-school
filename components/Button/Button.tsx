ee7fe2a48ca104c3b55215ed2e5ee78887f0369a
Anzhelika Karpik <karpik.lika@mail.ru>
2022-05-03 19:50:09 +0300

    Add HTMLProps to components

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -3,13 +3,14 @@ import cn from 'classnames';
 import styles from './Button.module.css';
 
 
-export const Button = ({appearance, children}: ButtonProps): JSX.Element => {
+export const Button = ({appearance, children, ...props}: ButtonProps): JSX.Element => {
     return(
         <button 
             className={cn(styles.button, {
                 [styles.primary]: appearance == 'primary',
                 [styles.ghost]: appearance == "ghost",
-            })}>
+            })}
+            {...props}>
             {children}
         </button>
     );
