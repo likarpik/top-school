d4c9c100fb857c45aeaef7f066da6b95a91a99e5
Anzhelika Karpik <karpik.lika@mail.ru>
2022-05-21 20:56:44 +0300

    Add layout styles using grid

--- layout/Header/Header.tsx
+++ layout/Header/Header.tsx
@@ -1,6 +1,4 @@
 import { HeaderProps } from "./Header.props";
-import cn from "classnames";
-import styles from "./Header.module.css";
 
 export const Header = ( {...props} : HeaderProps): JSX.Element => {
     return(
