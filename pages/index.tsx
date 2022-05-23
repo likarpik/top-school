401ac4a0ef97c6fbf6a952f390e713dadb4812ed
Anzhelika Karpik <karpik.lika@mail.ru>
2022-05-23 23:15:47 +0300

    Add server site rendering with using getStaticProps and getStaticPaths

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
+import { GetStaticProps } from "next";
 import { useState } from "react";
 import { Button, Htag, Paragraph, Rating, Tag } from "../components";
 import { withLayout } from "../layout/Layout";
+import axios from "axios";
+import { MenuItem } from "../interfaces/menu.interface";
 
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
   const [rating, setRating] = useState<number>(4);
 
   return (
@@ -23,9 +26,27 @@ function Home(): JSX.Element {
       <Tag size="small" color="green">Тег</Tag>
       <Tag color="ghost">Тег</Tag>
       <Rating rating={rating} isEditable setRating={setRating}/>
+      <ul>
+        {menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
+      </ul>
     </>
   );
 }
 
 export default withLayout(Home);
 
+export const getStaticProps: GetStaticProps<HomeProps> = async() => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {firstCategory});
+  return {
+    props: {
+      menu,
+      firstCategory
+    }
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
